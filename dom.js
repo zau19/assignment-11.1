3885437a1f4c0d5d1d8ed18a6a06616dfe6ef7c5
zau19 <54337274+zau19@users.noreply.github.com>
2019-09-11 22:15:41 -0400

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,8 @@
+var tag = document.getElementById("first");
+ console.log(tag);
+ var tag = document.getElementsByClassName("special");
+ console.log(tag);
+ var tag = document.getElementById("special");
+ console.log(tag);
+ var tag = document.getElementById("last");
+ console.log(tag);
